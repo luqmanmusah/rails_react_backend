6d23598dfe256be1b532332e71ffce57b17bd0fa
luqmanmusah <musahluqman@gmail.com>
2022-01-28 21:33:42 +0000

    Add random message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,10 @@
 class MessagesController < ApplicationController
   def index
   end
+  def random_message
+    prng = Random.new
+        ran_num = prng.rand(Message.count)
+        ran_msg = Message.all[ran_num]
+        render json: ran_msg
+  end
 end
