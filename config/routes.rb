b54c375903036ef7285ba34146e3a3871a8a40e4
luqmanmusah <musahluqman@gmail.com>
2022-01-28 21:34:16 +0000

    Add random message routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/hello_react'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :messages, only: %i[index]
-      get 'randomMessage', to: 'messages#random_message'
+
+  resources :messages, only: %i[index]
+  get 'randomMessage', to: 'messages#random_message'
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
